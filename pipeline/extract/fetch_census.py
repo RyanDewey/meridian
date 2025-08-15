21359e61bc9674d8faa9d25d75bb72ccc72cc015
Ryan Dewey <145182587+RyanDewey@users.noreply.github.com>
2025-08-15 11:56:41 -0700

    Fixed dataframe and added all zipcodes for OC

--- pipeline/extract/fetch_census.py
+++ pipeline/extract/fetch_census.py
@@ -2,7 +2,7 @@ import requests
 import pandas as pd
 
 api_key = 'f01cca6a59e13e855ac681716055ca2ad72ef2c6'
-population_base_url = 'https://api.census.gov/data/2023/acs/acs5?get=NAME,B01001_001E&for=zip%20code%20tabulation%20area:'
+population_base_url = 'https://api.census.gov/data/2023/acs/acs5?get=B01001_001E&for=zip%20code%20tabulation%20area:'
 demographic_base_url = ''
 
 # Returns dataframe with population data
@@ -12,7 +12,6 @@ def get_population_data(zip_codes):
     for zip_code in zip_codes[1:]:
         zip_codes_list += f",{zip_code}"
 
-    print(f"{population_base_url}{zip_codes_list}&key={api_key}")
 
     # Get population data from census api
     response = requests.get(f"{population_base_url}{zip_codes_list}&key={api_key}")
@@ -21,7 +20,10 @@ def get_population_data(zip_codes):
     data = response.json()
 
     # Store in dataframe
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(data[1:], columns=['population_total', 'zip_code'])
+
+    # Reorder columns to put zip_code first
+    df = df[['zip_code', 'population_total']]
 
     # Print dataframe for debugging
     print(df)
